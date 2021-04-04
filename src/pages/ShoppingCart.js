1e01a8047c838625bf72aea1625d824000716622
Sandy <sandyddinh@gmail.com>
2021-04-03 21:53:41 -0400

    styled the Shopping Cart page

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -26,35 +26,84 @@ export default function ShoppingCart({ history }) {
 		})();
 	}, []);
 
-	// const handleCheckout = e => {
-	// 	e.preventDefault();
-	// 	history.push('/checkout');
-	// };
-
 	return (
 		<div className="ShoppingCartPage">
 			{cart.length ? (
-				<div className="cart-container">
-					{cart.map(item => {
-						return (
-							<div key={item._id}>
-								<img src={item.product.image} />
-								<h4>Name: {item.product.name}</h4>
-								<h4>Product ID: {item.product.id}</h4>
-								<h4>Color: {item.product.color}</h4>
-								<h4>Size: {item.product.size}</h4>
-								<h4>Qty: {item.product.qty}</h4>
-								<h4>Item Price: {item.product.price}</h4>
-								<h4>Total Price: {item.product.price * item.product.qty}</h4>
-							</div>
-						);
-					})}
-					<h4>Total Price: ${cartTotal}</h4>
-					{/* <Link to={{ pathname: `/checkout`, state: cartTotal }}>Checkout</Link> */}
-					{/* <button type="button" onClick={handleCheckout}> */}
-					<Link to={{ pathname: '/checkout', state: cartTotal }}>Checkout</Link>
-					{/* </button> */}
-				</div>
+				<>
+					<div className="cart-container">
+						<h2>Basket</h2>
+						<hr />
+						<table>
+							<tr>
+								<th></th>
+								<th>
+									<p className="emphasize">Item</p>
+								</th>
+								<th>
+									<p className="emphasize">Item Price</p>
+								</th>
+								<th>
+									<p className="emphasize">Quantity</p>
+								</th>
+								<th>
+									<p className="emphasize">Total Price</p>
+								</th>
+							</tr>
+							{cart.map(item => {
+								return (
+									<tr key={item._id}>
+										<td>
+											<img src={item.product.image} className="product-image" />
+										</td>
+										<td>
+											<p className="emphasize">{item.product.name}</p>
+											<p>
+												<span className="emphasize">Style #</span>{' '}
+												{item.product.id}
+											</p>
+											<p>
+												<span className="emphasize">Color</span>{' '}
+												<span className="uppercase">{item.product.color}</span>
+											</p>
+											<p>
+												<span className="emphasize">Size</span>{' '}
+												{item.product.size}
+											</p>
+											<a href="#" className="remove-link">
+												Remove
+											</a>
+										</td>
+										<td>
+											<p>${item.product.price}</p>
+										</td>
+										<td>
+											<p>{item.product.qty}</p>
+										</td>
+										<td>
+											<p className="emphasize">
+												${item.product.price * item.product.qty}
+											</p>
+										</td>
+									</tr>
+								);
+							})}
+						</table>
+					</div>
+					<div className="order-summary">
+						<h2>Order Summary</h2>
+						<div className="order-details">
+							<p>Subtotal: ${cartTotal}</p>
+							<p>Shipping: TBD</p>
+							<p>Estimated Tax: $0</p>
+							<p className="emphasize">Total: ${cartTotal}</p>
+							<Link to={{ pathname: '/checkout', state: cartTotal }}>
+								<button type="button" className="checkout-button">
+									Checkout
+								</button>
+							</Link>
+						</div>
+					</div>
+				</>
 			) : (
 				<div>
 					<h2>Your cart is empty</h2>
