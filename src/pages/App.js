1306358db77f0835578a64035b71a35402340363
Sandy <sandyddinh@gmail.com>
2021-04-08 12:04:55 -0400

    added links to images on the App page

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,33 +4,22 @@ export default function App(props) {
 	return (
 		<div className="AppPage">
 			<div className="top-section">
-				<img src="https://res.cloudinary.com/dxuoqqfve/image/upload/v1617661481/cantaloupe/spring-collection_pz7qdd.jpg" />
+				<a href="/clothing">
+					<img src="https://res.cloudinary.com/dxuoqqfve/image/upload/v1617661481/cantaloupe/spring-collection_pz7qdd.jpg" />
+				</a>
 			</div>
 			<div className="bottom-section">
 				<div className="bottom-left-section">
-					<img src="https://res.cloudinary.com/dxuoqqfve/image/upload/v1617661481/cantaloupe/new-to-activewear_maex8z.jpg" />
+					<a href="/activewear">
+						<img src="https://res.cloudinary.com/dxuoqqfve/image/upload/v1617661481/cantaloupe/new-to-activewear_maex8z.jpg" />
+					</a>
 				</div>
 				<div className="bottom-right-section">
-					<img src="https://res.cloudinary.com/dxuoqqfve/image/upload/v1617661481/cantaloupe/winter-sale_wu0yaa.jpg" />
+					<a href="/sale">
+						<img src="https://res.cloudinary.com/dxuoqqfve/image/upload/v1617661481/cantaloupe/winter-sale_wu0yaa.jpg" />
+					</a>
 				</div>
 			</div>
-			{/* <ul>
-				<li>Add a shop now image and link</li>
-				<li>Add a section for new arrivals</li>
-				<li>Add a footer</li>
-				<li>Add Shopping Cart</li>
-				<li>Add Stripe Integration</li>
-				<li>Add Favorites?</li>
-			</ul>
-			The pages should be:
-			<ul>
-				<li>New Arrivals</li>
-				<li>Clothing</li>
-				<li>Activewear</li>
-				<li>Wellness</li>
-				<li>Sale</li>
-				<li>Gift Card - Maybe</li>
-			</ul> */}
 		</div>
 	);
 }
